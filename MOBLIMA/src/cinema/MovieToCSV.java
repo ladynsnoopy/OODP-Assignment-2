2aa0c7b761dd62bdd13d1d852db71e31c01a5ff5
espressodepresso <56994283+espressodepresso@users.noreply.github.com>
2019-11-03 17:39:55 +0800

    data updates into database without []

--- MOBLIMA/src/cinema/MovieToCSV.java
+++ MOBLIMA/src/cinema/MovieToCSV.java
@@ -3,19 +3,27 @@ package cinema;
 import java.util.ArrayList;
 
 public class MovieToCSV {
-	public static void addMovieToCSV(Movie movie) {
+	public void addMovieToCSV(Movie movie) {
 		ArrayList<String> data = new ArrayList<String>();
 		data.add(Integer.toString(movie.getMovieID()));
 		data.add(movie.getName());
 		data.add(movie.getType());
 		data.add(movie.getShowingStatus());
 		data.add(movie.getSynopsis());
-		data.add(movie.getDirector().toString());
-		data.add(movie.getCast().toString());
+		String directorData = movie.getDirector().toString();
+		directorData = directorData.substring(1, directorData.length()-1);
+		data.add(directorData);
+		String castData = movie.getCast().toString();
+		castData = castData.substring(1, castData.length()-1);
+		data.add(castData);
 		data.add(movie.getOverallUserRating());
 		data.add(Integer.toString(movie.getTicketSales()));
-		data.add(movie.getReviewIDs().toString());
-		data.add(movie.getShowtimeIDs().toString());
+		String reviewData = movie.getReviewIDs().toString();
+		reviewData = reviewData.substring(1, reviewData.length()-1);
+		data.add(reviewData);
+		String showtimeData = movie.getShowtimeIDs().toString();
+		showtimeData = showtimeData.substring(1, showtimeData.length()-1);
+		data.add(showtimeData);
 		data.add(movie.getMovieRating());
 		csvRW.writeToCSV("moviedatabase", data);
 	}
