639bc1bce5d412035bb2d45530916f9e668aefb0
teng-oh <juntengoh@gmail.com>
2019-11-03 17:57:31 +0800

    ahhh

--- MOBLIMA/src/cinema/csvTester.java
+++ MOBLIMA/src/cinema/csvTester.java
@@ -13,43 +13,23 @@ public class csvTester {
 //		addstuff.add("oh no, we are so bad, at this");
 //		csvRW.writeToCSV("staffdatabase", addstuff);
 		
-//		ArrayList<String> castlist = new ArrayList<String>();
-//		castlist.add("Michael Johnson");
-//		castlist.add("Jane Doe");
-//		ArrayList<String> directorlist = new ArrayList<String>();
-//		directorlist.add("Michael Bay");
-//		Review testreview = new Review(4,"Hello it's me","How have you been");
-//		ArrayList<Review> review = new ArrayList<Review>();
-//		review.add(testreview);
-//		ArrayList<Showtime> showtimelist = new ArrayList<Showtime>();
-//		Cineplex cine = new Cineplex("NTU");
-//		Cinema Ctest = new Cinema("normal",10,10,cine);
-//		Showtime test = new Showtime(Ctest,"7pm Sunday");
-//		showtimelist.add(test);
-//		Movie testermovie = new Movie("Test Movie","Showing","In which a movie is tested",castlist,directorlist,"Action",review,showtimelist,"PG13",0);
-//		MovieToCSV a = new MovieToCSV();
-//		a.addMovieToCSV(testermovie);
-		
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter movie name to edit: ");
-		String mv_name = sc.next();
-		ArrayList<String> result = csvRW.search("moviedatabase", "Title", mv_name);
-		String id = result.get(0);
-		
-		System.out.println("Movie details are:");
-		for(int i=0; i <result.size();i++) {
-			System.out.println(result.get(i));
-		}
-		
-		System.out.println("Enter column name to edit:");
-		String col_name = sc.next();
-		System.out.println("Enter new data to be inserted:");
-		String new_data = sc.next();
-		
-		csvRW.editCSV("moviedatabase", id , col_name , new_data);
-		
-		
+		ArrayList<String> castlist = new ArrayList<String>();
+		castlist.add("Michael Johnson");
+		castlist.add("Jane Doe");
+		ArrayList<String> directorlist = new ArrayList<String>();
+		directorlist.add("Michael Bay");
+		Review testreview = new Review(4,"Hello it's me","How have you been");
+		ArrayList<Review> review = new ArrayList<Review>();
+		review.add(testreview);
+		ArrayList<Showtime> showtimelist = new ArrayList<Showtime>();
+		Cineplex cine = new Cineplex("NTU");
+		Cinema Ctest = new Cinema("normal",10,10,cine);
+		Showtime test = new Showtime(Ctest,"7pm Sunday");
+		showtimelist.add(test);
+		Movie testermovie = new Movie("Test Movie","Showing","In which a movie is tested",castlist,directorlist,"Action","PG13");
+		MovieToCSV a = new MovieToCSV();
+		a.addMovieToCSV(testermovie);
+
 		
 
 //		
