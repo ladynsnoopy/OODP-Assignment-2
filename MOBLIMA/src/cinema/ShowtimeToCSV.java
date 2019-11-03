84465df5b98661b9400e0018e7183e1e4c907a34
teng-oh <juntengoh@gmail.com>
2019-11-03 12:28:11 +0800

    ACTUAL csv this time

---
+++ MOBLIMA/src/cinema/ShowtimeToCSV.java
@@ -0,0 +1,13 @@
+package cinema;
+
+import java.util.ArrayList;
+
+public class ShowtimeToCSV {
+	public void addShowtimeToCSV(Showtime showtime) {
+		ArrayList<String> data = new ArrayList<String>();
+		data.add(Integer.toString(showtime.getShowtimeID()));
+		data.add(showtime.getCinema().getCinemaID());
+		data.add(showtime.getTiming());
+		csvRW.writeToCSV("showtimedatabase", data);
+	}
+}
