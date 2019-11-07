ce34bb9c160b6514f0b617b2096fd9b3d970ed68
espressodepresso <56994283+espressodepresso@users.noreply.github.com>
2019-11-07 11:23:23 +0800

    formatting in moviedb

--- MOBLIMA/src/cinema/csvTester.java
+++ MOBLIMA/src/cinema/csvTester.java
@@ -31,24 +31,26 @@ public class csvTester {
 //		a.addMovieToCSV(testermovie);
 		
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter movie name to edit: ");
-		String mv_name = sc.next();
-		ArrayList<String> result = csvRW.search("moviedatabase", "Title", mv_name);
-		String id = result.get(0);
-		
-		System.out.println("Movie details are:");
-		for(int i=0; i <result.size();i++) {
-			System.out.println(result.get(i));
-		}
-		
-		System.out.println("Enter column name to edit:");
-		String col_name = sc.next();
-		System.out.println("Enter new data to be inserted:");
-		String new_data = sc.next();
-		
-		csvRW.editCSV("moviedatabase", id , col_name , new_data);
-		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter movie name to edit: ");
+//		String mv_name = sc.next();
+//		ArrayList<String> result = csvRW.search("moviedatabase", "Title", mv_name);
+//		String id = result.get(0);
+//		
+//		System.out.println("Movie details are:");
+//		for(int i=0; i <result.size();i++) {
+//			System.out.println(result.get(i));
+//		}
+//		
+//		System.out.println("Enter column name to edit:");
+//		String col_name = sc.next();
+//		System.out.println("Enter new data to be inserted:");
+//		String new_data = sc.next();
+//		
+//		csvRW.editCSV("moviedatabase", id , col_name , new_data);
+		ArrayList<String[]> moviedata = new ArrayList<String[]>(csvRW.readCSV("moviedatabase"));
+		String [] test = moviedata.get(1)[10].split(",");
+		System.out.println(test[1]);
 		
 		
 
