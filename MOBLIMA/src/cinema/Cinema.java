52aa66d7aa4b4035cf1974fac4c572361bf25329
teng-oh <juntengoh@gmail.com>
2019-11-09 16:26:52 +0800

    Added javadoc comments

--- MOBLIMA/src/cinema/Cinema.java
+++ MOBLIMA/src/cinema/Cinema.java
@@ -1,12 +1,45 @@
 package cinema;
 
+/**
+ * Cinema object that contains number of rows and columns of seats in the
+ * cinema, total number of seats and has a unique CinemaID
+ * 
+ * @author Jun Teng
+ * @version 1.0
+ * @since 2019-11-09
+ *
+ */
 public class Cinema {
+	/**
+	 * Type of cinema in String. Can be "Gold Class", "Normal".
+	 */
 	private String type;
+	/**
+	 * Total number of rows of seats in a cinema.
+	 */
 	private int totalRow;
+	/**
+	 * Total number of columns of seats in a cinema.
+	 */
 	private int totalCol;
+	/**
+	 * Total number of seats in a cinema.
+	 */
 	private int totalNumSeat;
+	/**
+	 * Unique String cinema ID.
+	 */
 	private String cinemaID;
 
+	/**
+	 * Constructor for cinema object.
+	 * 
+	 * @param type     Type of cinema in String. Can be "Gold Class", "Normal".
+	 * @param totalRow Total number of rows of seats in a cinema.
+	 * @param totalCol Total number of columns of seats in a cinema.
+	 * @param cineplex Total number of seats in a cinema.
+	 * @param cinemaID Unique String cinema ID.
+	 */
 	public Cinema(String type, int totalRow, int totalCol, Cineplex cineplex, String cinemaID) {
 		super();
 		this.type = type;
@@ -16,38 +49,74 @@ public class Cinema {
 		this.totalNumSeat = totalRow * totalCol;
 	}
 
+	/**
+	 * 
+	 * @return Type of cinema in String. Can be "Gold Class", "Normal".
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * 
+	 * @param type Type of cinema in String. Can be "Gold Class", "Normal".
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * 
+	 * @return Total number of rows of seats in a cinema.
+	 */
 	public int getTotalRow() {
 		return totalRow;
 	}
 
+	/**
+	 * 
+	 * @param totalRow Total number of rows of seats in a cinema.
+	 */
 	public void setTotalRow(int totalRow) {
 		this.totalRow = totalRow;
 	}
 
+	/**
+	 * 
+	 * @return Total number of columns of seats in a cinema.
+	 */
 	public int getTotalCol() {
 		return totalCol;
 	}
 
+	/**
+	 * 
+	 * @param totalCol Total number of columns of seats in a cinema.
+	 */
 	public void setTotalCol(int totalCol) {
 		this.totalCol = totalCol;
 	}
 
+	/**
+	 * 
+	 * @return Total number of seats in a cinema.
+	 */
 	public int getTotalNumSeat() {
 		return totalNumSeat;
 	}
 
+	/**
+	 * 
+	 * @param totalNumSeat Total number of seats in a cinema.
+	 */
 	public void setTotalNumSeat(int totalNumSeat) {
 		this.totalNumSeat = totalRow * totalCol;
 	}
 
+	/**
+	 * 
+	 * @return Unique String cinema ID.
+	 */
 	public String getCinemaID() {
 		return cinemaID;
 	}
--- MOBLIMA/src/cinema/Seat.java
+++ MOBLIMA/src/cinema/Seat.java
@@ -1,10 +1,34 @@
 package cinema;
 
+/**
+ * Seat object that contains x and y coordinates and occupied status for the
+ * seat.
+ * 
+ * @author Oh Jun Teng
+ * @version 1.0
+ * @since 2019-11-09
+ */
 public class Seat {
+	/**
+	 * X-coordinate of the seat
+	 */
 	private int xCoor;
+	/**
+	 * Y-coordinate of the seat
+	 */
 	private int yCoor;
+	/**
+	 * true if occupied, false if not
+	 */
 	private boolean isOccupied;
 
+	/**
+	 * Constructor for Seat object.
+	 * 
+	 * @param xCoor      x-coordinate location of seat
+	 * @param yCoor      y-coordinate location of seat
+	 * @param isOccupied true if occupied, false if not
+	 */
 	public Seat(int xCoor, int yCoor, boolean isOccupied) {
 		super();
 		this.xCoor = xCoor;
@@ -12,26 +36,44 @@ public class Seat {
 		this.isOccupied = isOccupied;
 	}
 
+	/**
+	 * @return x-coordinates.
+	 */
 	public int getxCoor() {
 		return xCoor;
 	}
 
+	/**
+	 * @param xCoor x-coordinate of seat
+	 */
 	public void setxCoor(int xCoor) {
 		this.xCoor = xCoor;
 	}
 
+	/**
+	 * @return y-coordinate of seat
+	 */
 	public int getyCoor() {
 		return yCoor;
 	}
 
+	/**
+	 * @param yCoor y-coordinate of seat
+	 */
 	public void setyCoor(int yCoor) {
 		this.yCoor = yCoor;
 	}
 
+	/**
+	 * @return true if occupied, false if not
+	 */
 	public boolean isOccupied() {
 		return isOccupied;
 	}
 
+	/**
+	 * @param isOccupied true if occupied, false if not
+	 */
 	public void setOccupied(boolean isOccupied) {
 		this.isOccupied = isOccupied;
 	}
