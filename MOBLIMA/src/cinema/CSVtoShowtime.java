8870d12b781abee4b15c465848fa63569b4d48d5
teng-oh <juntengoh@gmail.com>
2019-11-13 16:16:06 +0800

    New javadocs, removed more extra classes

--- MOBLIMA/src/cinema/CSVtoShowtime.java
+++ MOBLIMA/src/cinema/CSVtoShowtime.java
@@ -2,7 +2,22 @@ package cinema;
 
 import java.util.ArrayList;
 
+/**
+ * Control class to convert showtime entries in database to java objects.
+ * 
+ * @author Lim Wai Leong
+ * @version 1.0
+ * @since 2019-11-13
+ *
+ */
 public class CSVtoShowtime {
+	/**
+	 * Reads database and creates a <code>Showtime</code> object for every entry in
+	 * database.
+	 * 
+	 * @return <code>ArrayList&lt;Showtime&gt;</code> of created <code>Showtime</code>
+	 *         objects
+	 */
 	public static ArrayList<Showtime> csvToShowtimeObject() {
 		ArrayList<String[]> db = csvRW.readCSV("showtimedatabase");
 		ArrayList<Showtime> ShowtimeObjArr = new ArrayList<Showtime>();
@@ -12,16 +27,16 @@ public class CSVtoShowtime {
 			int showtimeID = Integer.parseInt(element[0]);
 			String cinema = element[1];
 			String timing = element[2];
-			int k=0;
+			int k = 0;
 			for (int j = 0; j < StaffApp.cinemaArr.size(); j++) {
 				if (StaffApp.cinemaArr.get(j).getCinemaID().contains(cinema)) {
-					k=j;
+					k = j;
 					break;
 				}
 			}
 			Showtime o = new Showtime(StaffApp.cinemaArr.get(k), timing);
 			o.setShowtimeID(showtimeID);
-			
+
 			ShowtimeObjArr.add(o);
 
 		}
