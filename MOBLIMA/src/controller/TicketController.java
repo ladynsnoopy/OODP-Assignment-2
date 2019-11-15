94cc1991878f0e4523f964c9c245bf0ec6bb8f1e
teng-oh <juntengoh@gmail.com>
2019-11-15 15:14:49 +0800

    Brand new docs yet again

--- MOBLIMA/src/controller/TicketController.java
+++ MOBLIMA/src/controller/TicketController.java
@@ -17,7 +17,7 @@ import model.Movie;
  * @author Myat Hmu Khin
  * @version 1.0
  * @since 2019-11-13
- * @see model.Ticket
+ * @see model.CurrentTicket
  * @see SeatingPlan
  */
 public class TicketController {
@@ -30,7 +30,7 @@ public class TicketController {
 	 * @param y          Y-coordinate of seat
 	 * @return -1 if seat is already occupied. <br>
 	 *         1 if purchase was successful.
-	 * @see model.Ticket
+	 * @see model.CurrentTicket
 	 */
 	public static int buyTicket(int showtimeID, int x, int y) {
 
@@ -79,7 +79,7 @@ public class TicketController {
 	 * @param isAdult    Selection between age categories. 1 for child, 2 for adult
 	 *                   and 3 for senior prices.
 	 * @return Newly created <code>Ticket</code> object.
-	 * @see model.Ticket
+	 * @see model.CurrentTicket
 	 * @see Movie#increaseTicketSalesByOne()
 	 */
 	public static CurrentTicket addTicket(int showtimeID, String movietitle, int isAdult) {
