3dbac35f7308a859e9a64c48c4a0aa1786c86d3b
teng-oh <juntengoh@gmail.com>
2019-11-15 17:15:31 +0800

    minor bug fix

--- MOBLIMA/src/view/DisplayUserPage.java
+++ MOBLIMA/src/view/DisplayUserPage.java
@@ -209,7 +209,7 @@ public class DisplayUserPage extends DisplayPageAb {
 					System.out.println("No showtimes are available yet.");
 					return;
 				}
-				for (int i = 0; i < showtimes.length; i++) {
+				for (int i = 1; i < showtimes.length; i++) {
 					String showstring = showtimes[i][1];
 					System.out.println("Showtime ID: " + showtimes[i][0]);
 					System.out.println("Time and Date: " + showstring.substring(0, 2) + "-" + showstring.substring(2, 4)
